74b83bfd9e1a826e017287335ed63b6154f51f4e
Tanveer Dar <tanveerdar163@gmail.com>
2023-05-09 07:24:25 +0530

    Update PrimeNumber.java

--- PrimeNO/src/PrimeNumber.java
+++ PrimeNO/src/PrimeNumber.java
@@ -1,6 +1,6 @@
         import java.util.Scanner;
 
-        public class PrimeNumber {
+        public class Sum {
             public static void main(String[] args) {
                 Scanner sc = new Scanner(System.in);
                 int sum=0;
